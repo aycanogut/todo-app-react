c45b00525d18a20eebbc9539cdf776af36bb385f
aycanogut <aycanonfire@gmail.com>
2021-11-19 15:56:31 +0300

    add form component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import Normalize from 'react-normalize';
 
 import { GlobalStyles } from './components/globalStyles';
 
+import Form from './components/Form/Form';
+
 function App() {
   return (
     <div className="App">
       <Normalize />
       <GlobalStyles />
+      <Form />
     </div>
   );
 }
