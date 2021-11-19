8f2a45a3c877dbbccf443fe37c391b795689b2bc
aycanogut <aycanonfire@gmail.com>
2021-11-19 15:57:09 +0300

    add styled, proptypes and props

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,11 +1,16 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import PropTypes from "prop-types";
 
 const StyledIcon = styled.img`
   /* css */
 `;
 
-const Icon = () => {
-  return <StyledIcon>ToDo List</StyledIcon>;
+const Icon = ({ source }) => {
+  return <StyledIcon src={source} />;
+};
+
+Icon.propTypes = {
+  source: PropTypes.string.isRequired
 };
 
 export default Icon;
