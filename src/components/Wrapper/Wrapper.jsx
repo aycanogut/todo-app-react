5eaad26295fa8307f0d3b7433eff48acb0f0fb6a
aycanogut <aycanonfire@gmail.com>
2021-11-19 18:37:29 +0300

    add wrapper component

---
+++ src/components/Wrapper/Wrapper.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+import helpers from "../helpers";
+
+import Form from "../Form/Form";
+import Brand from "../Brand/Brand";
+
+const StyledWrapper = styled.div`
+  max-width: 590px;
+  margin: 40px auto 0 auto;
+  color: ${helpers.colors.white};
+
+  @media only screen and ${helpers.device.md} {
+    margin: 90px auto 0 auto;
+  } ;
+`;
+
+const Wrapper = () => {
+  return (
+    <>
+      <Brand />
+      <StyledWrapper>
+        <Form />
+      </StyledWrapper>
+    </>
+  );
+};
+
+export default Wrapper;
