3a1dc4a1990c411e940cedee0963aa68aebf35b1
aycanogut <aycanonfire@gmail.com>
2021-11-19 13:47:45 +0300

    add globalStyles and normalize

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Normalize from 'react-normalize';
+
+import { GlobalStyles } from './components/globalStyles';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Normalize />
+      <GlobalStyles />
+    </div>
+  );
 }
 
 export default App;
